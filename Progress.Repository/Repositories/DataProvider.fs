d325fff850372b9cd4acd04854639e7bb7a8b2b3
Winter Lavigne <winter@ziekelingskes.eu>
2019-01-28 16:24:50 +0100

    Update DataProvider.fs

--- Progress.Repository/Repositories/DataProvider.fs
+++ Progress.Repository/Repositories/DataProvider.fs
@@ -4,5 +4,5 @@ open FSharp.Data.Sql
 
 type sql  = SqlDataProvider<
                    Common.DatabaseProviderTypes.MSSQLSERVER, 
-                   "Server=.,1433;Initial Catalog=Progress;Persist Security Info=False;User ID=SA;Password=W3nt2r!Docker);MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=10;"
+                   "Server=.,1433;Initial Catalog=Progress;Persist Security Info=False;User ID={user};Password={password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=10;"
                    >
